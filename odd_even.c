4540815a5fc870da815f63efc76703e0bc15687a
Masudha Meher <55777560+samflab@users.noreply.github.com>
2019-10-05 00:57:24 +0530

    Add files via upload

---
+++ odd_even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter the value of n");
+    scanf("%d"n);
+    if(n%2=0)
+    {
+        print("The number is even");
+    }
+    else(n%2=1)
+    {
+        Printf("The number is odd");
+    }
+}
